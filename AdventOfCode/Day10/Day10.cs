3e3ad58071ebcac584cce624d87dc05652dd7fca
Petr Kubát <petr.kubat@gmail.com>
2015-12-21 22:12:24 +0100

    Basic structure for Day 10.

---
+++ AdventOfCode/Day10/Day10.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode.Shared;
+using System;
+
+namespace AdventOfCode.Day10
+{
+    public class Day10 : IAdventDay
+    {
+        public string SolvePartOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SolvePartTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PuzzleName => "Elves Look, Elves Say";
+    }
+}
---
+++ AdventOfCodeTests/Day10Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCodeTests
+{
+    [TestFixture]
+    public class Day10Tests
+    {
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
