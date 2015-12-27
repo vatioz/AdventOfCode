a4056ad99f756f3138b374a5d565c0335b4e79d6
Petr Kubát <petr.kubat@gmail.com>
2015-12-27 19:01:07 +0100

    Day 12 structure

---
+++ AdventOfCode/Day12/Day12.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day12
+{
+    public class Day12 : IAdventDay
+    {
+        public string PuzzleName => "JSAbacusFramework.io";
+
+        public string SolvePartOne()
+        {
+            return "Not yet";
+        }
+
+        public string SolvePartTwo()
+        {
+            return "Not yet";
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day12Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeTests
+{
+    [TestFixture]
+    public class Day12Tests
+    {
+        [Test]
+        public void Test()
+        {
+                
+        }
+    }
+}
